f3e6818919a6cfaaaad7aea2c78c156c03d855a7
bkoshelev <brskoshelev@gmail.com>
2022-06-22 15:41:37 +0300

    solutions for part 3 tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const pathKeys = path.split('.');
+    return (obj) => {
+        let currentValue = obj;
+        for (let i = 0; i < pathKeys.length; i++) {
+            if (currentValue) {
+                currentValue = currentValue[pathKeys[i]];
+            } else {
+                return;
+            }
+        }
+        return currentValue;
+    };
 }
