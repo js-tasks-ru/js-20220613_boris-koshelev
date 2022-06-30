eb4c561791ee7d35694f61eccfc83fe79cc46f98
bkoshelev <brskoshelev@gmail.com>
2022-06-30 11:24:42 +0300

    solutions for part 5 tasks

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
-export default class NotificationMessage {
+const MS_TO_SEC = 1000;
+const DELAY = 100;
+const DEFAULT_DURATION = 200;
 
+export default class NotificationMessage {
+  static isAlreadyRunning = false;
+  static currentInstance = null;
+
+  constructor(
+    text = "",
+    { type = "success", duration = DEFAULT_DURATION } = {}
+  ) {
+    this.type = type;
+    this.duration = duration;
+    this.text = text;
+
+    this.render();
+  }
+
+  get template() {
+    const durationInSecond = this.duration / MS_TO_SEC;
+    return /* HTML */ ` <div
+      class="notification ${this.type}"
+      style="--value:${durationInSecond}s"
+    >
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">${this.text}</div>
+      </div>
+    </div>`;
+  }
+
+  show(renderElement = null) {
+    if (NotificationMessage.isAlreadyRunning) {
+      clearTimeout(NotificationMessage.currentInstance.timeoutRunner);
+      NotificationMessage.currentInstance.remove();
+      NotificationMessage.isAlreadyRunning = false;
+    }
+
+    if (renderElement) {
+      this.element = renderElement;
+    }
+
+    NotificationMessage.currentInstance = this;
+    this.element.removeAttribute("hidden");
+    NotificationMessage.isAlreadyRunning = true;
+
+    this.timeoutRunner = setTimeout(() => {
+      this.remove();
+      NotificationMessage.isAlreadyRunning = false;
+    }, this.duration - DELAY);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  render() {
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = this.template;
+    this.element = tempElement.firstElementChild;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,143 @@
+const sortStrings = (str1, str2, direction) => {
+  let [compareStr1, compareStr2] = [str1, str2];
+  if (direction === "desc") {
+    [compareStr1, compareStr2] = [compareStr2, compareStr1];
+  }
+
+  return compareStr1.localeCompare(compareStr2, ["ru", "en"], {
+    caseFirst: "upper",
+  });
+};
+
+const sortNumbers = (num1, num2, direction) => {
+  if (direction === "asc") {
+    return num1 - num2;
+  } else {
+    return num2 - num1;
+  }
+};
+
+const sortByType = {
+  number: sortNumbers,
+  string: sortStrings,
+};
+
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
 
+    this.render();
+  }
+
+  get header() {
+    return this.headerConfig
+      .map(({ id, title, sortable }) => {
+        return /* HTML */ ` <div
+          class="sortable-table__cell"
+          data-id="${id}"
+          data-sortable="${sortable}"
+        >
+          <span>${title}</span>
+        </div>`;
+      })
+      .join("");
+  }
+
+  get body() {
+    return this.data
+      .map((tableRowData) => {
+        const row = this.headerConfig
+          .map(({ id, sortType }) => {
+            let content = null;
+            if (sortType === "number") {
+              content = tableRowData[id];
+            }
+            if (sortType === "string") {
+              content = tableRowData[id];
+            }
+
+            return `<div class="sortable-table__cell">${content}</div>`;
+          })
+          .join("");
+
+        return `<a class="sortable-table__row">${row}</a>`;
+      })
+      .join("");
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  sort(field, direction = "asc") {
+    const headerData = this.headerConfig.find(
+      (headerData) => headerData.id === field
+    );
+
+    this.data.sort((firstRowData, secondRowData) => {
+      let [sortElem1, sortElem2] = [firstRowData[field], secondRowData[field]];
+
+      return sortByType[headerData.sortType](sortElem1, sortElem2, direction);
+    });
+
+    this.subElements.body.innerHTML = this.body;
+  }
+
+  get template() {
+    return /* HTML */ `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div
+            data-element="header"
+            class="sortable-table__header sortable-table__row"
+          ></div>
+          <div data-element="body" class="sortable-table__body"></div>
+          <div
+            data-element="loading"
+            class="loading-line sortable-table__loading-line"
+          ></div>
+          <div
+            data-element="emptyPlaceholder"
+            class="sortable-table__empty-placeholder"
+          >
+            <div>
+              <p>No products satisfies your filter criteria</p>
+              <button type="button" class="button-primary-outline">
+                Reset all filters
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getSubElements() {
+    const result = {};
+
+    const elements = this.element.querySelectorAll("[data-element]");
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  render() {
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = this.template;
+    this.element = tempElement.firstElementChild;
+
+    this.subElements = this.getSubElements();
+
+    this.subElements.header.innerHTML = this.header;
+    this.subElements.body.innerHTML = this.body;
   }
 }
-
